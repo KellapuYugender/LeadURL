e4ba01218762efa3c8b9376938d0fb28717177bd
KellapuYugender <kellapuyugender@gmail.com>
2026-05-14 19:38:16 +0530

    Date storing in Local storage

--- index.js
+++ index.js
@@ -6,17 +6,27 @@ const saveTabEl = document.getElementById("savetab-id");
 const deleteEl = document.getElementById("delete-id");
 const ulEl = document.getElementById("ul-id");
 
+const localStorageData = JSON.parse(localStorage.getItem("myLeads"));
+
+if (localStorageData) {
+  myLeads = localStorageData;
+  render(myLeads);
+}
+
 saveInputEl.addEventListener("click", function () {
   myLeads.push(inputEl.value);
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
   render(myLeads);
   inputEl.value = "";
 });
 
 deleteEl.addEventListener("dblclick", function () {
   myLeads = [];
+  localStorage.clear();
   render(myLeads);
   inputEl.value = "";
 });
+
 function render(leads) {
   let listItems = "";
   for (let i = 0; i < leads.length; i++) {
